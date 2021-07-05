fa2df21b73863e901534317b9cc5546fdc7a9c9e
Mohammad <57011800+Mammad10@users.noreply.github.com>
2021-07-05 09:14:00 +0300

    Updates

---
+++ precache-manifest.f78f1cf106dd86ca6511efe04ae6adf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f364dead3e7cf1ea5449183f0377693",
+    "url": "/github.io/navbar/index.html"
+  },
+  {
+    "revision": "81fe813a058cee366e3e",
+    "url": "/github.io/navbar/static/css/main.d1743f3b.chunk.css"
+  },
+  {
+    "revision": "5036664f6a0cc62b8b04",
+    "url": "/github.io/navbar/static/js/2.41d20619.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/navbar/static/js/2.41d20619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81fe813a058cee366e3e",
+    "url": "/github.io/navbar/static/js/main.d0dad195.chunk.js"
+  },
+  {
+    "revision": "6696f64156b96ffe9d62",
+    "url": "/github.io/navbar/static/js/runtime-main.2f7855cd.js"
+  },
+  {
+    "revision": "2b686f2e5682570fb85fc952a4581419",
+    "url": "/github.io/navbar/static/media/logo.2b686f2e.svg"
+  }
+]);
